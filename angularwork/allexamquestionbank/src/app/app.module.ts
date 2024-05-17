de59675edc1110ca4e2d927b3c085ea1c20e6839
sunny0009rajput <gateece493@gmail.com>
2024-05-17 13:01:31 +0530

    allexamagular work

---
+++ angularwork/allexamquestionbank/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { BodyComponent } from './component/body/body.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    BodyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
