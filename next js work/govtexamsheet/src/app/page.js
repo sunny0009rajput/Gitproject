eb20bf0c47acf3fcf41ae46d1b9efe5f105cac49
sunny0009rajput <gateece493@gmail.com>
2025-10-12 20:08:55 +0530

    next js work

---
+++ next js work/govtexamsheet/src/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Navbar from "./Navbar";
+import { Container } from "lucide-react";
+
+export default function Home() {
+  return (
+    <>
+    <div className="min-h-screen bg-black">
+    <Navbar/>
+    </div>
+    
+    </>
+  );
+}
