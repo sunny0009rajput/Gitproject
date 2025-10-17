5c04057810231be54e4acebcc99712764c371abd
sunny0009rajput <gateece493@gmail.com>
2025-10-18 01:07:17 +0530

    json data

--- pythonworks/main.py
+++ pythonworks/main.py
@@ -2,7 +2,7 @@ import fitz  # PyMuPDF
 import json
 import re
 
-def extract_pdf_to_custom_json(pdf_path, output_json="first.json"):
+def extract_pdf_to_custom_json(pdf_path, output_json="bio.json"):
     pdf_document = fitz.open(pdf_path)
     all_text = ""
 
@@ -83,4 +83,4 @@ def extract_pdf_to_custom_json(pdf_path, output_json="first.json"):
 
 
 # Example usage
-extract_pdf_to_custom_json("C:/Users/sunny/Downloads/firsttext.pdf")
+extract_pdf_to_custom_json("C:/Users/sunny/Downloads/biotext.pdf")
